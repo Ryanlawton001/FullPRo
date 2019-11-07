c6a36f5a4dc88bb78b994b5122397dfce6095147
Ryanlawton001 <56266188+Ryanlawton001@users.noreply.github.com>
2019-11-07 14:00:12 +0000

    Add files via upload

---
+++ src/searcher/CleverSearcher.java
@@ -0,0 +1,35 @@
+package searcher;
+
+import java.util.Arrays;
+
+public class CleverSearcher extends Searcher
+{
+
+
+    CleverSearcher(int[] array, int k) {
+        super(array, k);
+    }
+
+
+    @Override
+    public int findElement() throws IndexingError {
+        int[] array = getArray();
+        int k = getIndex();
+        if (k <= 0 ||k > array.length) {
+            throw new IndexingError();
+
+        }
+        int[] smallArray = new int[k];
+        for (int index = 0; index < k; index++){
+            smallArray[index] = array[index];
+        }
+        Arrays.sort(smallArray);
+        for (int index = k; index <array.length; index++) {
+            if (array[index] > smallArray[0]){
+                smallArray[0] = array[index];
+                Arrays.sort(smallArray);
+            }
+        }
+        return smallArray[0];
+    }
+}
